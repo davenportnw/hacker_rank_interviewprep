200d28df3642c12104628c2d6bae1bd108c933f7
davenportnw <davenportnw@gmail.com>
2020-10-17 20:45:31 -0500

    Creating array to string to number.

--- Arrays/leftrotation.js
+++ Arrays/leftrotation.js
@@ -10,30 +10,34 @@ function rotLeft(a,d) {
     
     //Locate the position in which the first element will go to.
     function findLocation(a) {
-         //Use a for loop to keep track going through the array.
+         
+        //Use a for loop to keep track going through the array.
         for(let i=0; i<a.length; i++) {
-        //Subtract the current position given the number of times to rotate.
-        let locate = 3
-        let newPosition = i - locate;
-        console.log('newPosition', newPosition);
-        
-        //If the new position is less than zero, add the number to the end of the array.
-        // if(newPosition < 0) {
-            
-            
-        // }
-            
-        function lastItem () {
-            let lastItem = a.slice(-1);
-            console.log('lastitem', lastItem);
-            
-            return lastItem
-        }
+            // console.log('currentPosition', i);
+            //Subtract the current position given the number of times to rotate.
+            let locate = 4
+            let newPosition = i - locate;
+            // console.log('newPosition', newPosition);
 
         
+            //Find the last item of the element 
+            function lastItem (age) {
+                let lastItem = a.slice(-1);
+                let lastItemString = lastItem.toString();
+                let lastItemNumber = parseInt(lastItemString);
+                console.log('lastitemNumber', lastItemNumber);
+                return age === lastItemNumber
+            }
+            //Find the index of the last element
             let indexOf = a.findIndex(lastItem);
             console.log('indexOf', indexOf);
-        
+
+            //If the new position is less than zero, add the number to the end of the array.
+            // if(newPosition < 0) {
+            //     let newIndex = newPosition + indexOf;
+            //     console.log('newIndex', newIndex);
+
+            // }
         }
         
     }
